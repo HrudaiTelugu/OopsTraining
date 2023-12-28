09374005a95fdcfa0b616999efa3bb74e277c0d9
HrudaiTelugu <hrudaitelugu@gmail.com>
2023-12-28 08:09:24 -0600

    Pushing this to Github

---
+++ InheritanceConcepts.java
@@ -0,0 +1,13 @@
+package oops.concepts;
+
+public class InheritanceConcepts {
+
+	public static void main(String[] args) {
+		System.out.println("Hi git This is My Second Time to push in GitHub ");
+		System.out.println("I will Create the code From GitHub ");
+		System.out.println("Thankyou");
+		System.out.println("Pulling this Line from GitHub");
+
+	}
+
+}
